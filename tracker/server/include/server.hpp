386756b25eb29011d1ddb1f921c4350eab6ee14d
kirill <kirillturov20012013@gmail.com>
2020-04-03 15:36:17 +0300

    Update main logic

--- tracker/server/include/server.hpp
+++ tracker/server/include/server.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
-#include <cstdlib>
-#include <iostream>
+//#include <cstdlib>
 #include <boost/asio.hpp>
 #include <set>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 namespace UDP_server {
     using boost::asio::ip::udp;
@@ -11,29 +13,46 @@ namespace UDP_server {
     constexpr std::size_t HASH_SIZE = 20;
     constexpr std::size_t CONNECT_REQUEST_SIZE = 16;
     constexpr std::size_t ANNOUNCE_REQUEST_SIZE = 98;
+    constexpr int32_t DEFAULT_ANNOUNCE_PEERS = 50;
+    constexpr int32_t MAX_ANNOUNCE_PEERS = 150;
+    using Hash_t = std::array<uint8_t, HASH_SIZE>;
+
+    /* структура из STL. как std::set, только с random_access за O(log n)
+     * https://codeforces.com/blog/entry/11080
+     */
+    template <typename T>
+    using  ordered_set = __gnu_pbds::tree<T,
+                                          __gnu_pbds::null_type,
+                                          std::less<>,
+                                          __gnu_pbds::rb_tree_tag,
+                                          __gnu_pbds::tree_order_statistics_node_update>;
 
     class Peer {
     public:
-        Peer() = default;
-        explicit Peer(udp::endpoint endpoint);
+        //Peer() = default;
+        //explicit Peer(const Hash_t &peer_id, udp::endpoint endpoint);
         uint32_t ip() const noexcept;
         uint16_t port() const noexcept;
-        udp::endpoint ep() const noexcept;
-        bool operator<(const Peer &other) const noexcept; // для std::set
-    private:
-        udp::endpoint endpoint_;
-        std::array<uint8_t, HASH_SIZE> peer_id_{};
+        bool operator<(const Peer &other) const noexcept;
+
+    public:
+        udp::endpoint ep;
+        Hash_t peer_id{};
+        int32_t downloaded;
+        int32_t left;
+        int32_t uploaded;
+        bool completed = false;
     };
 
     class Torrent {
     public:
         Torrent() = default;
-        explicit Torrent(const std::array<uint8_t, HASH_SIZE> &hash);
-        std::array<uint8_t, HASH_SIZE> hash_{};
-        std::set<Peer> seeders_;
-        std::set<Peer> leechers_;
-        //может вообще хранить один set  и сортировать по кол-ву скачанный байт?
-        size_t downloads_ = 0;
+        explicit Torrent(const Hash_t &hash);
+        Hash_t info_hash{}; //unused
+        ordered_set<Peer> all_peers{};
+        std::size_t leechers = 0;
+        std::size_t seeders = 0;
+        std::size_t downloads = 0;
     };
 
     enum class ActionType : int32_t {
@@ -49,20 +68,16 @@ namespace UDP_server {
         int32_t transaction_id;
 
         //if action ==  1 or 2
-        std::vector<std::array<uint8_t, HASH_SIZE>> info_hashes;
+        std::vector<Hash_t> info_hashes;
 
         //if action == 1
-        std::array<uint8_t, HASH_SIZE> peer_id;
-        int64_t downloaded;
-        int64_t left;
-        int64_t uploaded;
+        Peer sender;
         EventType event;
         uint32_t ip;
         uint32_t key;
         int32_t num_want;
-        uint16_t port;
 
-        Peer sender;
+        uint16_t port;
         std::string error_message;
     };
 
@@ -105,12 +120,17 @@ namespace UDP_server {
         Response handle_announce(const Request &request);
         Response handle_scrape(const Request &request);
         Response handle_error(const Request &request);
+        int32_t count_leechers(const Hash_t &info_hash);
+        int32_t count_seeders(const Hash_t &info_hash);
+        int32_t count_downloads(const Hash_t &info_hash);
+        std::vector<Peer> get_peer_list(const Hash_t &info_hash, int32_t num_want);
+        void update_peer_list(const Hash_t &info_hash, const Peer &peer, EventType event);
         void print_request(const Request &request);
         void print_response(const Response &response);
     private:
         udp::socket socket_;
         int32_t request_interval_;
         bool silent_mode_;
-        std::map<std::array<uint8_t, HASH_SIZE>, Torrent> torrents_;
+        std::map<Hash_t, Torrent> torrents_;
     };
 } //namespace UDP_server
--- tracker/server/src/server.cpp
+++ tracker/server/src/server.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <boost/endian/conversion.hpp>
+#include <random>
+#include <utility>
 
 #include "server.hpp"
 
@@ -15,33 +18,23 @@ namespace UDP_server {
     using namespace boost::asio;
     using namespace boost::endian;
 
-    Peer::Peer(udp::endpoint endpoint) :
-            endpoint_(std::move(endpoint)) {}
+    //Peer::Peer(const Hash_t &peer_id, udp::endpoint endpoint) :
+    //        endpoint(std::move(endpoint)), peer_id(peer_id) {}
 
     uint32_t Peer::ip() const noexcept {
-        return endpoint_.address().to_v4().to_ulong();
+        return ep.address().to_v4().to_ulong();
     }
 
     uint16_t Peer::port() const noexcept {
-        return endpoint_.port();
-    }
-
-    udp::endpoint Peer::ep() const noexcept {
-        return endpoint_;
+        return ep.port();
     }
 
     bool Peer::operator<(const Peer &other) const noexcept {
-        if (ip() != other.ip())
-            return ip() < other.ip();
-        else
-            return port() < other.port();
+        return peer_id < other.peer_id;
     }
 
-    Torrent::Torrent(const std::array<uint8_t, HASH_SIZE> &hash) :
-            hash_(hash),
-            seeders_(),
-            leechers_(),
-            downloads_(0) {}
+    Torrent::Torrent(const Hash_t &hash) :
+            info_hash(hash) {}
 
     Server::Server(boost::asio::io_context &io_context,
                    uint16_t port,
@@ -55,7 +48,7 @@ namespace UDP_server {
         for (;;) {
             std::vector<uint8_t> message(PACKET_SIZE);
             udp::endpoint sender_endpoint;
-            size_t message_length = socket_.receive_from(boost::asio::buffer(message), sender_endpoint);
+            std::size_t message_length = socket_.receive_from(boost::asio::buffer(message), sender_endpoint);
             message.resize(message_length);//?
             Request request = parse_UDP_request(message, sender_endpoint);
             print_request(request);
@@ -77,7 +70,7 @@ namespace UDP_server {
 
             print_response(response);
 
-            socket_.send_to(boost::asio::buffer(make_UDP_response(response), message_length), response.sender.ep());
+            socket_.send_to(boost::asio::buffer(make_UDP_response(response), message_length), response.sender.ep);
         }
     }
 
@@ -88,7 +81,7 @@ namespace UDP_server {
     }
 
     template<>
-    void load_value(std::array<uint8_t, HASH_SIZE> &value, const uint8_t *&source) {
+    void load_value(Hash_t &value, const uint8_t *&source) {
         std::copy(source, source + sizeof(value), value.data());
         source += sizeof(value);
     }
@@ -128,11 +121,12 @@ namespace UDP_server {
 
             request.info_hashes.emplace_back();
             load_value(request.info_hashes.back(), iter);
+            request.sender.ep = ep;
+            load_value(request.sender.peer_id, iter);
+            load_value(request.sender.downloaded, iter);
+            load_value(request.sender.left, iter);
+            load_value(request.sender.uploaded, iter);
 
-            load_value(request.peer_id, iter);
-            load_value(request.downloaded, iter);
-            load_value(request.left, iter);
-            load_value(request.uploaded, iter);
             load_value(request.event, iter);
             if (EventType::NONE > request.event || request.event > EventType::STOPPED) {
                 request.error_message = "Bad request: bad Event field";
@@ -145,7 +139,7 @@ namespace UDP_server {
         }
 
         if (request.action == ActionType::SCRAPE) {
-            size_t bytes_left = message.size() - CONNECT_REQUEST_SIZE;
+            std::size_t bytes_left = message.size() - CONNECT_REQUEST_SIZE;
             if (bytes_left == 0 || bytes_left % HASH_SIZE != 0) {
                 request.error_message = "Bad request: incomplete hash for Scrape";
                 return request;
@@ -158,7 +152,6 @@ namespace UDP_server {
             }
         }
 
-        request.sender = Peer(ep);
         return request;
     }
 
@@ -220,38 +213,12 @@ namespace UDP_server {
 
         response.interval = request_interval_;
 
-        if (torrents_.find(request.info_hashes[0]) == torrents_.end()) {
-            torrents_[request.info_hashes[0]] = Torrent(request.info_hashes[0]);
-        }
+        const Hash_t &info_hash = request.info_hashes[0];
+        response.leechers = count_leechers(info_hash);
+        response.seeders = count_seeders(info_hash);
+        response.peers = get_peer_list(info_hash, request.num_want);
 
-        auto &seeders_set = torrents_[request.info_hashes[0]].seeders_;
-        auto &leechers_set = torrents_[request.info_hashes[0]].leechers_;
-
-        response.leechers = leechers_set.size();
-        response.seeders = seeders_set.size();
-        //incorrect, should use num_want ->
-        for (const auto &peer : seeders_set) {
-            response.peers.push_back(peer);
-        }
-        //incorrect, should use num_want ->
-        for (const auto &peer : leechers_set) {
-            response.peers.push_back(peer);
-        }
-
-        if (request.event == EventType::NONE) {
-            //nothing
-        }
-        if (request.event == EventType::COMPLETED) {
-            leechers_set.erase(request.sender);
-            seeders_set.insert(request.sender);
-        }
-        if (request.event == EventType::STARTED) {
-            leechers_set.insert(request.sender);
-        }
-        if (request.event == EventType::STOPPED) {
-            leechers_set.erase(request.sender);
-            seeders_set.erase(request.sender);
-        }
+        update_peer_list(info_hash, request.sender, request.event);
 
         return response;
     }
@@ -263,14 +230,10 @@ namespace UDP_server {
         response.sender = request.sender;
 
         for (const auto &cur_info_hash : request.info_hashes) {
-            if (torrents_.find(cur_info_hash) == torrents_.end()) {
-                torrents_[cur_info_hash] = Torrent(cur_info_hash);
-            }
-
             response.scrape_info.emplace_back();
-            response.scrape_info.back().complete = torrents_[cur_info_hash].seeders_.size();
-            response.scrape_info.back().downloaded = torrents_[cur_info_hash].downloads_;
-            response.scrape_info.back().incomplete = torrents_[cur_info_hash].leechers_.size();
+            response.scrape_info.back().complete = count_seeders(cur_info_hash);
+            response.scrape_info.back().downloaded = count_downloads(cur_info_hash);
+            response.scrape_info.back().incomplete = count_leechers(cur_info_hash);
         }
 
         return response;
@@ -285,6 +248,95 @@ namespace UDP_server {
         response.error_message = request.error_message;
 
         return response;
+    }
+
+    int32_t Server::count_leechers(const Hash_t &info_hash) {
+        if (torrents_.find(info_hash) == torrents_.end()) {
+            torrents_[info_hash] = Torrent(info_hash);
+        }
+        return torrents_[info_hash].leechers;
+    }
+
+    int32_t Server::count_seeders(const Hash_t &info_hash) {
+        if (torrents_.find(info_hash) == torrents_.end()) {
+            torrents_[info_hash] = Torrent(info_hash);
+        }
+        return torrents_[info_hash].seeders;
+    }
+
+    int32_t Server::count_downloads(const Hash_t &info_hash) {
+        if (torrents_.find(info_hash) == torrents_.end()) {
+            torrents_[info_hash] = Torrent(info_hash);
+        }
+        return torrents_[info_hash].downloads;
+    }
+
+    std::mt19937 rnd;// где должно быть,?
+    std::vector<Peer> Server::get_peer_list(const Hash_t &info_hash, int32_t num_want) {
+        std::vector<Peer> peers;
+
+        if (torrents_.find(info_hash) == torrents_.end()) {
+            torrents_[info_hash] = Torrent(info_hash);
+        }
+
+        const auto &peer_set = torrents_[info_hash].all_peers;
+
+        if (num_want <= 0) {
+            num_want = DEFAULT_ANNOUNCE_PEERS;
+        }
+        num_want = std::min(num_want, MAX_ANNOUNCE_PEERS);
+        num_want = std::min(num_want, (int32_t)peer_set.size());
+
+        std::set<Peer> selected;
+        while ((int32_t)selected.size() < num_want) {
+            selected.insert(*peer_set.find_by_order(rnd() % peer_set.size()));
+        }
+        for (auto &peer : selected) {
+            peers.push_back(peer);
+        }
+        return peers;
+    }
+
+    void Server::update_peer_list(const Hash_t &info_hash, const Peer &peer, EventType event) {
+        if (torrents_.find(info_hash) == torrents_.end()) {
+            torrents_[info_hash] = Torrent(info_hash);
+        }
+
+        auto &torrent = torrents_[info_hash];
+        auto old_peer_ptr = torrent.all_peers.find(peer);
+
+        if (old_peer_ptr == torrent.all_peers.end() && event != EventType::STARTED) {
+            //bad
+        }
+        if (event == EventType::NONE) {
+            //nothing
+        }
+        if (event == EventType::COMPLETED) {
+            if (old_peer_ptr->completed) {
+                //bad
+            }
+            torrent.seeders++;
+            torrent.downloads++;
+            torrent.leechers--;
+
+            auto new_peer = *old_peer_ptr;
+            new_peer.completed = true;
+
+            torrent.all_peers.erase(old_peer_ptr);
+            torrent.all_peers.insert(new_peer);
+        }
+        if (event == EventType::STARTED) {
+            torrent.leechers++;
+            torrent.all_peers.insert(peer);
+        }
+        if (event == EventType::STOPPED) {
+            if (old_peer_ptr->completed) {
+                torrent.seeders--;
+            } else {
+                torrent.leechers--;
+            }
+            torrent.all_peers.erase(old_peer_ptr);
+        }
     }
 
     void Server::print_request(const Request &request) {
@@ -326,7 +378,7 @@ namespace UDP_server {
                     break;
             }
         }
-        info_message << " from " << request.sender.ep().address().to_string()
+        info_message << " from " << request.sender.ep.address().to_string()
                      << " " << request.sender.port() << '\n';
 
         std::cout << info_message.str();
@@ -353,7 +405,7 @@ namespace UDP_server {
                 break;
         }
 
-        info_message << " to " << response.sender.ep().address().to_string()
+        info_message << " to " << response.sender.ep.address().to_string()
                      << " " << response.sender.port() << '\n';
 
         std::cout << info_message.str();
