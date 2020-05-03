c2f26d9fdf172d49a0906120d5771135123cfabf
Ilya <smirok123@mail.ru>
2020-05-03 20:01:05 +0300

    name fix

--- client/source/View.cpp
+++ client/source/View.cpp
@@ -1,15 +1,15 @@
 #include "View.h"
 
-void View::updateTorrents(const std::vector<lt::torrent_status>& st) {
+void View::updateTorrents(const std::vector<lt::torrent_status>& statuses) {
     bool need_update = false;
-    for (auto& t : st){
-        if (t.download_rate == 0)
+    for (auto& status : statuses){
+        if (status.download_rate == 0)
             continue;
-        auto j = session_handles.find(t.handle);
-        if (j != session_handles.end()){
-            auto handle = t.handle;
-            session_handles[handle] = t;
-            current_handles.push_back(std::make_unique<lt::torrent_status const>(t));
+        auto updatable_status = session_handles.find(status.handle);
+        if (updatable_status != session_handles.end()){
+            auto handle = status.handle;
+            session_handles[handle] = status;
+            current_handles.push_back(std::make_unique<lt::torrent_status const>(status));
             need_update = true;
         }
     }
@@ -20,18 +20,18 @@ void View::updateTorrents(const std::vector<lt::torrent_status>& st) {
 
 void View::updateCurrentTorrents() {
     current_handles.clear();
-    for (auto &h : session_handles){
-        if (h.first.is_valid())
-            current_handles.push_back(std::make_unique<lt::torrent_status const>(h.second));
+    for (auto &handle : session_handles){
+        if (handle.first.is_valid())
+            current_handles.push_back(std::make_unique<lt::torrent_status const>(handle.second));
     }
 
-    for (auto i = current_handles.begin();
-         i != current_handles.end();){
-        lt::torrent_status const& s = **i;
-        if (!s.handle.is_valid()){
-            i = current_handles.erase(i);
+    for (auto iter = current_handles.begin();
+         iter != current_handles.end();){
+        lt::torrent_status const& status = **iter;
+        if (!status.handle.is_valid()){
+            iter = current_handles.erase(iter);
             continue;
         }
-        ++i;
+        ++iter;
     }
 }
